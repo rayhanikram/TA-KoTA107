a8bb04d5b55379462d51164aab545691de61334e
rayhanikram <rayhan.ikram.tif17@polban.ac.id>
2020-09-20 18:44:05 +0700

    Fitur Mapsummarize Selesai 2

--- Backend/src/geocoder.py
+++ Backend/src/geocoder.py
@@ -21,7 +21,7 @@ Kecamatan= None
 
 loc = [...]*len(df)
 def GeocodePY(i):
- locator = GoogleV3(api_key='AIzaSyAVrV_E0qgmnH30D5pfZ4j5LXpuESZfkTU')
+ locator = GoogleV3(api_key='Hello World')
  loc= df.loc[i,'Address']
  location = locator.geocode(loc,bounds=[-7.032068,107.534180,-6.848032,107.745323])
  try:
